01c98bfc640eeb1f6e3bac8ce7eaf013153bb4df
FelipeFOS <cxfosfelipe@gmail.com>
2018-04-19 21:57:16 -0300

    Agenda de Alunos - First Commit.

---
+++ Agenda/app/src/main/java/br/com/felipedossantos/curso/agenda/FormularioActivity.java
@@ -0,0 +1,93 @@
+package br.com.felipedossantos.curso.agenda;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+import br.com.felipedossantos.curso.agenda.dao.AlunoDAO;
+import br.com.felipedossantos.curso.agenda.modelo.Aluno;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    public static final int CODE_INTENT_CAMERA = 1;
+    private FormularioHelper helper;
+    private String caminhoFoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        helper = new FormularioHelper (this);
+
+        Intent intent = getIntent();
+        Aluno aluno = (Aluno) intent.getSerializableExtra("aluno");
+        if (aluno != null) {
+            helper.preencheFormulario(aluno);
+        }
+
+        Button botaoFoto = findViewById(R.id.formulario_botao_foto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intentCamera, CODE_INTENT_CAMERA);
+                caminhoFoto = getExternalFilesDir(null) + "/"+ System.currentTimeMillis() +".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                startActivity(intentCamera);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CODE_INTENT_CAMERA) {
+                helper.carregaImagem(caminhoFoto);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
+                Aluno aluno = helper.pegaAluno();
+                AlunoDAO dao = new AlunoDAO(this);
+
+                if (aluno.getId() == null){
+                    dao.insere(aluno);
+                } else {
+                    dao.altera(aluno);
+                }
+                dao.close();
+
+                Toast.makeText(FormularioActivity.this, "Aluno " + aluno.getNome() + " salvo!", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Agenda/app/src/main/java/br/com/felipedossantos/curso/agenda/ListaAlunosActivity.java
@@ -0,0 +1,151 @@
+package br.com.felipedossantos.curso.agenda;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import br.com.felipedossantos.curso.agenda.dao.AlunoDAO;
+import br.com.felipedossantos.curso.agenda.modelo.Aluno;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    public static final int CODE_PERM_CALL = 1;
+    private ListView listaAlunos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+
+        listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> lista, View item, int position, long id) {
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(position);
+
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                intentVaiProFormulario.putExtra("aluno", aluno);
+                startActivity(intentVaiProFormulario);
+            }
+        });
+
+        Button novoAluno = (Button) findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
+
+        registerForContextMenu(listaAlunos);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        carregaLista();
+    }
+
+    private void carregaLista() {
+        AlunoDAO dao = new AlunoDAO(this);
+        List<Aluno> alunos = dao.buscaalunos();
+        dao.close();
+
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        // Capturando informações do menu de contexto acionado
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        // Extraido posição do item do menu de contexto acionado e com base nisto capturado o objeto do Aluno
+            final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+        // Acidionado itens ao Menu de Contexto
+            MenuItem itemLigar = menu.add("Ligar");
+            MenuItem itemSMS = menu.add("SMS");
+            MenuItem itemSite = menu.add("Visitar Site");
+            MenuItem itemMapa = menu.add("Ver End. no Mapa");
+            MenuItem itemDeletar = menu.add("Deletar");
+
+        //Ação do Item de Ligar
+            itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    if (ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(ListaAlunosActivity.this,
+                                new String[]{Manifest.permission.CALL_PHONE}, CODE_PERM_CALL);
+                    } else {
+                        Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                        intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+                        startActivity(intentLigar);
+                    }
+                    return false;
+                }
+            });
+
+        // Ação do Item de Envio de SMS
+            Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+            intentSMS.setData(Uri.parse("sms:" + aluno.getTelefone()));
+            itemSMS.setIntent(intentSMS);
+
+        // Ação do Item de acesso ao Site do Aluno
+            Intent intentSite = new Intent(Intent.ACTION_VIEW);
+            String site = aluno.getSite();
+            if(!site.startsWith("http://") & !site.startsWith("https://")) {
+                site = "http://" + site;
+            }
+            intentSite.setData(Uri.parse(site));
+            itemSite.setIntent(intentSite);
+
+        // Ação de Item de visualização do endereço no Mapa
+            Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+            intentMapa.setData(Uri.parse("geo:0,0?z=14&q=" + aluno.getEndereco()));
+            itemMapa.setIntent(intentMapa);
+
+        // Ação do Item de Deletar
+            itemDeletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
+                    dao.deleta(aluno);
+                    dao.close();
+
+                    carregaLista();
+
+                    Toast.makeText(ListaAlunosActivity.this, "Deletado o Aluno " + aluno.getNome() + "!", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        // Metodo de retorno a cada solicitação de permissão, aqui que deve ser tratado o que fará logo após o usuário definir se permite ou não.
+        // requestCode tem o Int passado no metodo ActivityCompat.requestPermissions
+        // resquestCode criados no fonte:
+        // 1 = CODE_PERM_CALL = Acesso para realizar Chamada Telefonica do Aluno.
+    }
+}
